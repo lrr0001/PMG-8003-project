422157807c4d26daadb55145eef3c38a990a3220
Zhu, Yuanda <yzhu94@gatech.edu>
2018-04-12 03:41:57 -0400

    added trial3

--- house_votes_data.py
+++ house_votes_data.py
@@ -18,33 +18,33 @@ for k in range(0,len(lines)):
     this_line = lines[k]
     this_ele = this_line.split(",")
     if this_ele[0] == "republican":
-        votes[k,0] = 0
+        votes[k,0] = -1
     elif this_ele[0] == "democrat":
         votes[k,0] = 1
         
     for kk in range(1,17):
-        if this_ele[kk] == "n":
-            votes[k,kk] = 0
-        elif this_ele[kk] == "y":
-            votes[k,kk] = 1
-        elif this_ele[kk] == "?":
+        if this_ele[kk] == "n" or this_ele[kk] == "n\n":
             votes[k,kk] = -1
+        elif this_ele[kk] == "y" or this_ele[kk] == "y\n":
+            votes[k,kk] = 1
+        elif this_ele[kk] == "?" or this_ele[kk] == "?\n":
+            votes[k,kk] = 0
             
-new_votes = votes[:]
+clean_votes = votes[:]
 k = 0
-while k < np.size(new_votes,0):
-    if any(new_votes[k,:] == -1):
-#        np.delete(new_votes,new_votes[k,:])
-        new_votes = np.delete(new_votes,k,0)
+while k < np.size(clean_votes,0):
+    if any(clean_votes[k,:] == 0):
+        clean_votes = np.delete(clean_votes,k,0)
     else:
         k = k + 1;
         
 num_dem = 0
 num_rep = 0
-for k in range(0,np.size(new_votes,0)):
-    if new_votes[k,0] == 0:
+for k in range(0,np.size(clean_votes,0)):
+    if clean_votes[k,0] == -1:
         num_rep = num_rep + 1
-    elif new_votes[k,0] == 1:
+    elif clean_votes[k,0] == 1:
         num_dem = num_dem + 1;
         
-np.save('house_votes',votes,new_votes)
+np.save('house_votes_total',votes)
+np.save('house_votes_clean',clean_votes)
